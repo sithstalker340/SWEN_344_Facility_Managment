e675cd309abafa136a6991c4f626db244cc01239
Alexander Kidd <debatical@gmail.com>
2017-03-21 20:37:39 -0400

    POST and GET mockup operations work for device
    -Use this format for POST (params not required yet, however):
    http://localhost:3000/device/1
    -I <3 the Postman app

--- FacMan/routes/device.js
+++ FacMan/routes/device.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET classroom info */
-router.get('/:id', function(req, res, next) {
-	var device = {
-		id : req.params.id,
-		room : req.params.classroom,
-		student_id : req.params.student_id,
-    student_name : req.params.student_name,
-    reserved : req.params.reserved,
-    condition: req.params.condition,
-    return_date : req.params.return_date
-	};
-	res.json(device);
-});
+/* Device API */
+router.route('/:id')
+
+  .post(function(req, res) {
+      res.send('device successfully created.');
+  })
+
+  .get(function(req, res, next) {
+	    var device = {
+		      id : req.params.id,
+		      room : req.params.classroom,
+		      student_id : req.params.student_id,
+          student_name : req.params.student_name,
+          reserved : req.params.reserved,
+          condition: req.params.condition,
+          return_date : req.params.return_date
+	    };
+	    res.json(device);
+  });
 
 module.exports = router;
