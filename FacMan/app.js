22cb0226d48cbe7a0002bc0a630ebacaa79a3dee
Andrew Vogler <axv3658@rit.edu>
2017-04-11 14:56:34 -0400

    Fixed routing, added devices page. Commented out forms on index.html leave them for now

--- FacMan/app.js
+++ FacMan/app.js
@@ -5,11 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var classroom = require('./routes/classroom');
-var device = require('./routes/device');
-
 var app = express();
 
 // view engine setup
@@ -24,10 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/classroom', classroom);
-app.use('/device', device);
+// routing
+app.use(require('./routes'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- FacMan/routes/devices.js
+++ FacMan/routes/devices.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/devices', require('./devices'));
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('devices');
 });
-
 
 module.exports = router;
