e09f5e4077a8c37b3294195ef0891ef9bf97ad9b
Alexander Kidd <debatical@gmail.com>
2017-04-14 12:52:20 -0400

    Commit 1 for issue #16
    -Added basic footer
    -Updated head and header to have bootstrap and necessary
    bootstrap/jquery
    -Updated index links
    -Updated stylesheets

--- FacMan/app.js
+++ FacMan/app.js
@@ -11,7 +11,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'facman_favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
