dc4e649c2affd7f8fe7574b047c8e485199d5cfa
nns3455 <nns3455@cs.rit.edu>
2017-04-18 13:57:09 -0400

    Basic classrooms stuff

--- FacMan/routes/index.js
+++ FacMan/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/devices', require('./devices'));
+router.use('/classrooms', require('./classrooms'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
