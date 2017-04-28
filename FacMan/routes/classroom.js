63652a4f918580bbd1b1ff2977805bbba931565e
Adam Audycki <apa7395@rit.edu>
2017-04-28 17:53:01 -0400

    Added the page for individual classrooms so that when /classroom/id is entered it will get the needed information. The merge between the classroom and the courses that have reserved the classroom needs to be changed. Currently it just combines them into a single object. Also the API needs to be updated in order to have the getClassroomReservations added to the main repo.

--- FacMan/routes/classroom.js
+++ FacMan/routes/classroom.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var extend = require('extend');
 
-router.get('/', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
+	var urlC = "http://vm344f.se.rit.edu/API/API.php?function=getClassroom&team=facility_management&id=" + req.params.id;
+    var urlR = "http://vm344f.se.rit.edu/API/API.php?function=getClassroomReservations&team=facility_management&id=" + req.params.id;
+    var classroom;
+    var reserve;
 
-    res.render('classroom');
+    var callbackR = function(error, response, body) {
+        console.log(body);
+        reserve = JSON.parse(body);	
+        
+        var data = extend({}, classroom, reserve);
+        res.render('classroom', {"classroom" : data});
+    }
+    
+    var callbackC = function(error, response, body) {
+       console.log(body);
+       classroom = JSON.parse(body);	
+        
+        request(urlR,callbackR);
+	};
+    
+	request(urlC, callbackC);
+    
+    
 });
 
 module.exports = router;
