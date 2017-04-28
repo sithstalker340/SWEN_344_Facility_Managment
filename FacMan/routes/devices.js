17c99704f6aa49076d0c89bdbc73a001c1d0bf41
Andrew Vogler <axv3658@rit.edu>
2017-04-28 14:54:27 -0400

    Refactored API url to global variable

--- FacMan/routes/devices.js
+++ FacMan/routes/devices.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 router.get('/', function(req, res, next) {
-  res.render('devices');
+	var url = api + "function=getDevices&team=facility_management";
+	var callback = function(error, response, body) {
+		var devices = JSON.parse(body);
+		res.render('devices', {"devices" : devices});
+	};
+	request(url, callback);
+});
+
+router.get('/:id', function(req, res, next) {
+	// res.json(classroom);
+	console.log("Not implemented");
 });
 
 module.exports = router;
