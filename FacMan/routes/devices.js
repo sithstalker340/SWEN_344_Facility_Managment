245117191a288b4c899707fa126d38a0b0a7fcb5
Andrew Vogler <axv3658@rit.edu>
2017-05-02 08:44:26 -0400

    added routing for individual devices, started work on posting updates to a device

--- FacMan/routes/devices.js
+++ FacMan/routes/devices.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/', function(req, res, next) {
-	var url = api + "function=getDevices&team=facility_management";
+	var url = api + "team=facility_management&function=getDevices";
 	var callback = function(error, response, body) {
 		var devices = JSON.parse(body);
 		res.render('devices', 
@@ -14,9 +14,39 @@ router.get('/', function(req, res, next) {
 	request(url, callback);
 });
 
-router.get('/:id', function(req, res, next) {
-	// res.json(classroom);
-	console.log("Not implemented");
+router.route('/:id')
+.get(function(req, res, next) {
+	var url = api + "team=facility_management&function=getDevice&id=" + req.params.id;
+	
+	var idCallback = function(error, response, body){
+		device = JSON.parse(body);
+		res.render('device', 
+			{
+				device : device
+			});
+	};
+
+	request(url, idCallback);
+})
+.post(function(req, res, next){
+	var url = api + "team=facility_management&function=updateDevice";
+	var date = new Date();
+	var data = {
+		id : req.params.id,
+		condition : device.condition,
+		checkedOut : 1,
+		checkoutDate : date,
+		name : device.name,
+		userId : 1
+	};
+
+	// request.post({
+	// 	url : url,
+	// 	form : data,
+	// }, function(error, respose, body){
+	// 	console.log(body);
+	// });
 });
+
 
 module.exports = router;
