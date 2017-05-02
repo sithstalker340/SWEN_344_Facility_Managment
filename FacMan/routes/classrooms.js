b2c5895e1e359afb987ff000ffe91604d241505a
Adam Audycki <apa7395@rit.edu>
2017-05-02 13:59:13 -0400

    Added my changes that I made while Nick was combining the classroom stuff. I cleaned up the array that is sent through the page request.

--- FacMan/routes/classrooms.js
+++ FacMan/routes/classrooms.js
@@ -3,31 +3,29 @@ var router = express.Router();
 var request = require('request');
 var extend = require('extend');
 
-router.get('/:id', function(req, res, next) {
+router.get('classroom/:id', function(req, res, next) {
 	var urlC = "http://vm344f.se.rit.edu/API/API.php?function=getClassroom&team=facility_management&id=" + req.params.id;
     var urlR = "http://vm344f.se.rit.edu/API/API.php?function=getClassroomReservations&team=facility_management&id=" + req.params.id;
-    var classroom;
-    var reserve;
-
+    var data = [];
     var callbackR = function(error, response, body) {
         console.log(body);
-        reserve = JSON.parse(body);	
+        data['reserve'] = JSON.parse(body);	
         
-        var data = extend({}, classroom, reserve);
+        console.log(data);
         res.render('classroom', {"classroom" : data});
     }
     
     var callbackC = function(error, response, body) {
        console.log(body);
-       classroom = JSON.parse(body);	
+       data['classroom'] = JSON.parse(body);	
         
-        request(urlR,callbackR);
+       request(urlR,callbackR);
 	};
     
 	request(urlC, callbackC);
     
     
-});
+}); 
 
 
 /* GET classroom list */
