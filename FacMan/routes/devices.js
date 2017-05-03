e34448f9acd76f8c144569452a2845fe345627ae
Andrew Vogler <axv3658@rit.edu>
2017-05-03 12:04:59 -0400

    Added form for creating new devices. No functionality, WIP

--- FacMan/routes/devices.js
+++ FacMan/routes/devices.js
@@ -2,51 +2,93 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.get('/', function(req, res, next) {
-	var url = api + "team=facility_management&function=getDevices";
-	var callback = function(error, response, body) {
-		var devices = JSON.parse(body);
-		res.render('devices', 
-			{
-				"devices" : devices
-			});
-	};
-	request(url, callback);
-});
+router.route('/')
+	.get(function(req, res, next) {
+		var url = api + "team=facility_management&function=getDevices";
+		var callback = function(error, response, body) {
+			var devices = JSON.parse(body);
+			res.render('devices', 
+				{
+					devices : devices
+				});
+		};
+		request(url, callback);
+	});
 
-router.route('/:id')
-.get(function(req, res, next) {
-	var url = api + "team=facility_management&function=getDevice&id=" + req.params.id;
+router.route('/newDevice')
+	.get(function(req, res, next){
+		res.render('newDevice', { complete : false });
+	})
+	.post(function(req, res, next){
+		var url = api + "team=facility_management&function=addDevice";
+		var data = { name: req.body.name, condition: req.body.condition };
+
+		request.post({
+			url: url,
+			form: data
+		}, function(error, response, body){
+			if(error){
+				console.log('error', error);
+			}
+
+			res.render('newDevice', { complete : true });
+		});
+	});
+
+// router.get('/newDevice', function(req, res, next) {
+// 	res.render('newDevice');
+// });
+
+// router.post('/newDevice', function(req, res, next){
+// 	var url = api + "team=facility_management&function=addDevice";
+// 	var data = {
+// 		name: "test Name",
+// 		condition: "test cond"
+// 	};
+
+// 	request.post({
+// 		url : url,
+// 		form : data
+// 	}, function(error, response, body){
+// 		if(error){
+// 			console.log('error', error);
+// 		}
+// 		console.log("done");
+// 	});
+// });
+
+// router.get('/:id', function(req, res, next) {
+// 	var url = api + "team=facility_management&function=getDevice&id=" + req.params.id;
 	
-	var idCallback = function(error, response, body){
-		device = JSON.parse(body);
-		res.render('device', 
-			{
-				device : device
-			});
-	};
+// 	request(url, function(error, response, body){
+// 		device = JSON.parse(body);
 
-	request(url, idCallback);
-})
-.post(function(req, res, next){
-	var url = api + "team=facility_management&function=updateDevice";
-	var date = new Date();
-	var data = {
-		id : req.params.id,
-		condition : device.condition,
-		checkedOut : 1,
-		checkoutDate : date,
-		name : device.name,
-		userId : 1
-	};
+// 		res.render('device', 
+// 			{
+// 				device : device
+// 			});
+// 	});
+// });
 
-	// request.post({
-	// 	url : url,
-	// 	form : data,
-	// }, function(error, respose, body){
-	// 	console.log(body);
-	// });
-});
 
+// router.post('/:id', function(req, res, next){
+// 	var url = api + "team=facility_management&function=updateDevice";
+// 	var date = new Date();
+// 	var data = {
+// 		id : req.params.id,
+// 		condition : "test cond",
+// 		checkedOut : 1,
+// 		checkoutDate : date,
+// 		name : "test name",
+// 		userId : '1'
+// 	};
+
+// 	request.post({
+// 		url : url,
+// 		form : data,
+// 	}, function(error, respose, body){
+
+// 	});
+// });
 
 module.exports = router;
