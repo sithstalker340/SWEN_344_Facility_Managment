ef96dcf8835a4ad3beb592b62624d201ab49e480
Andrew Vogler <axv3658@rit.edu>
2017-05-08 13:23:55 -0400

    initial work on logging in

--- FacMan/routes/index.js
+++ FacMan/routes/index.js
@@ -13,5 +13,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.route('/login')
+	.get(function(req, res, next){
+		res.render('login');
+	})
+	.post(function(req, res, next){
+		var url = api + "team=facility_management&function=addDevice";
+		var data = "name=" + req.body.name + "&condition=" + req.body.condition;
+
+		// request.post({
+		// 	headers: {'content-type' : 'application/x-www-form-urlencoded'},
+		// 	url: url,
+		// 	body: data
+		// }, function(error, response, body){
+		// 	afterPost(error, req, res);
+		// }
+	// );
+	res.render('classrooms');
+});
+
 
 module.exports = router;
