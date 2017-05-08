214f62bad1e976d2dd1fd67f88ba7c64d03fb44a
Andrew Vogler <axv3658@rit.edu>
2017-05-08 18:39:10 -0400

    Continued work on registration/login

--- FacMan/routes/index.js
+++ FacMan/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var users = {};
+
 /* Declare the API url once, and reference this var in other routes */
 //api = "http://vm344f.se.rit.edu/API/API.php?"; //SWITCH TO THIS WHEN WE ARE DONE
 api = "https://www.se.rit.edu/~axv3658/project/API/API.php?"; //TESTING API
@@ -18,7 +20,38 @@ router.route('/')
 		var url = api + "team=general&function=login";
 		var data = "username=" + req.body.username + "&password=" + req.body.password;
 
-		console.log(data);
+		request.post({
+			headers: {'content-type' : 'application/x-www-form-urlencoded'},
+			url: url,
+			body: data
+		}, function(error, response, body){
+			if(error){
+				console.log('error', error)
+			}
+
+			if(response && response.statusCode){
+				console.log(response.statusCode);
+				res.redirect('http://' + req.get('host') + '/classrooms');
+			}
+		}
+	);
+});
+
+router.route('/register')
+	.get(function(req, res, next){
+		var url = api + "team=general&function=getUsers";
+		request.get(
+			url, function(error, response, body){
+				users = JSON.parse(body);
+				res.render('register', { users : users });
+			}
+		);
+	})
+	.post(function(req, res, next){
+		var url = api + "team=general&function=createUser";
+		var data = "username=" + req.body.username + "&password=" + req.body.password +
+					"&fname=" + req.body.fname + "&lname=" + req.body.lname + 
+					"&email=" + req.body.email + "&role=" + req.body.role;
 
 		request.post({
 			headers: {'content-type' : 'application/x-www-form-urlencoded'},
@@ -29,19 +62,13 @@ router.route('/')
 				console.log('error', error)
 			}
 
-			console.log(body);
-			res.redirect('http://' + req.get('host') + '/devices');
+			if(response && response.statusCode){
+				console.log(response.statusCode);
+				res.redirect('http://' + req.get('host') + '/classrooms');
+			}
 		}
 	);
 });
-
-router.route('/register')
-	.get(function(req, res, next){
-		res.render('index', {title : 'test'});
-	})
-	.post(function(req, res, next){
-		
-	});
 
 
 module.exports = router;
