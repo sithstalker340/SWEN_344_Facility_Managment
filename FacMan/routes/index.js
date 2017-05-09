7cf2f2ab87311bb39746634b40c6c6c59c3153d1
Andrew Vogler <axv3658@rit.edu>
2017-05-09 15:07:59 -0400

    Trying to add sessions for logging in

--- FacMan/routes/index.js
+++ FacMan/routes/index.js
@@ -5,7 +5,7 @@ var request = require('request');
 var users = {};
 
 /* Declare the API url once, and reference this var in other routes */
-//api = "http://vm344f.se.rit.edu/API/API.php?"; //SWITCH TO THIS WHEN WE ARE DONE
+// api = "http://vm344f.se.rit.edu/API/API.php?"; //SWITCH TO THIS WHEN WE ARE DONE
 api = "https://www.se.rit.edu/~axv3658/project/API/API.php?"; //TESTING API
 
 router.use('/devices', require('./devices'));
@@ -14,12 +14,12 @@ router.use('/classrooms', require('./classrooms'));
 /* GET home page. */
 router.route('/')
 	.get(function(req, res, next){
-		res.render('index', {title : 'Facility Management'});
+		res.render('index', {title : 'Facility Management', loginError : false});
 	})
 	.post(function(req, res, next){
 		var url = api + "team=general&function=login";
 		var data = "username=" + req.body.username + "&password=" + req.body.password;
-
+		
 		request.post({
 			headers: {'content-type' : 'application/x-www-form-urlencoded'},
 			url: url,
@@ -29,9 +29,13 @@ router.route('/')
 				console.log('error', error)
 			}
 
-			if(response && response.statusCode){
-				console.log(response.statusCode);
+			if(response && response.body !== "null"){
+				user = JSON.parse(body);
+				req.session.uid = user.ID;
+				req.session.test = "testing";
 				res.redirect('http://' + req.get('host') + '/classrooms');
+			} else {
+				res.render('index', {loginError : true});
 			}
 		}
 	);
