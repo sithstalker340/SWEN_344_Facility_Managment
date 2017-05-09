7b49621f4821d1775280acb6bc1652ad8ef8ad83
Andrew Vogler <axv3658@rit.edu>
2017-05-09 14:00:07 -0400

    removed registration flow

--- FacMan/routes/index.js
+++ FacMan/routes/index.js
@@ -37,54 +37,5 @@ router.route('/')
 	);
 });
 
-router.route('/register')
-	.get(function(req, res, next){
-		var url = api + "team=general&function=getUsers";
-		request.get(
-			url, function(error, response, body){
-				users = JSON.parse(body);
-				console.log(users);
-				res.render('register', {hasError : false, error : ''});
-			}
-		);
-	})
-	.post(function(req, res, next){
-		var url = api + "team=general&function=createUser";
-		var data = "username=" + req.body.username + "&password=" + req.body.password +
-					"&fname=" + req.body.fname + "&lname=" + req.body.lname + 
-					"&email=" + req.body.email + "&role=" + req.body.role;
-
-		request.get(
-			api + "team=general&function=getUsers", function(error, response, body){
-				users = JSON.parse(body);
-			}
-		);
-		var user = users.filter(function ( obj ) {
-		    return obj.USERNAME == req.body.username;
-		})[0];
-
-		console.log(user);
-
-		if(user === undefined || user === null){
-			request.post({
-				headers: {'content-type' : 'application/x-www-form-urlencoded'},
-				url: url,
-				body: data
-			}, function(error, response, body){
-				if(error){
-					console.log('error', error)
-				}
-
-				if(response && response.statusCode){
-					console.log(response.statusCode);
-					res.redirect('http://' + req.get('host') + '/classrooms');
-				}
-			});
-		} else {
-			res.render('register', {hasError: true, error : 'Username already exists. Please select a different username.'});
-		}
-
-});
-
 
 module.exports = router;
