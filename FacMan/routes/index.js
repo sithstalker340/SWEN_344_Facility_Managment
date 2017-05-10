8c0b5f408bcdd137e302c6d4da7fffe2762a7ce8
nns3455 <nns3455@cs.rit.edu>
2017-05-10 09:43:49 -0400

    prod api link

--- FacMan/routes/index.js
+++ FacMan/routes/index.js
@@ -5,8 +5,8 @@ var request = require('request');
 var users = {};
 
 /* Declare the API url once, and reference this var in other routes */
-// api = "http://vm344f.se.rit.edu/API/API.php?"; //SWITCH TO THIS WHEN WE ARE DONE
-api = "https://www.se.rit.edu/~axv3658/project/API/API.php?"; //TESTING API
+api = "http://vm344f.se.rit.edu/API/API.php?"; //SWITCH TO THIS WHEN WE ARE DONE
+// api = "https://www.se.rit.edu/~axv3658/project/API/API.php?"; //TESTING API
 
 /* GET home page. */
 router.route('/')
