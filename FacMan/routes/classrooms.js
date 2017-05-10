7fdf73c544e798e879d9925460700e67ff9ae001
Andrew Vogler <axv3658@rit.edu>
2017-05-09 20:32:35 -0400

    Added adding a classroom

--- FacMan/routes/classrooms.js
+++ FacMan/routes/classrooms.js
@@ -3,6 +3,40 @@ var router = express.Router();
 var request = require('request');
 var extend = require('extend');
 
+/* GET classroom list */
+router.get('/', function (req, res, next) {
+    var url = api + "function=getClassrooms&team=facility_management";
+    var callback = function (error, response, body) {
+        var classrooms = JSON.parse(body);
+        res.render('classrooms', {"classrooms": classrooms});
+    };
+    request(url, callback);
+});
+
+router.route('/newClassroom')
+    .get(function (req, res, next) {
+        res.render('newClassroom');
+    })
+    .post(function (req, res, next) {
+
+    var url = api + "team=facility_management&function=addClassroom";
+    var data = "building=" + req.body.building_num + "&capacity=" + req.body.capacity +
+                "&room=" + req.body.room_num;
+
+    request.post({
+            headers: {'content-type': 'application/x-www-form-urlencoded'},
+            url: url,
+            body: data
+        }, function (error, response, body) {
+            if (error) {
+                console.log('error', error)
+            }
+
+            res.redirect('http://' + req.get('host') + '/classrooms/');
+        }
+    );
+});
+
 router.get('/:id', function (req, res, next) {
 
     var urlC = api + "function=getClassroom&team=facility_management&id=" + req.params.id;
@@ -30,21 +64,12 @@ router.get('/:id', function (req, res, next) {
 
 });
 
-/* GET classroom list */
-router.get('/', function (req, res, next) {
-    var url = api + "function=getClassrooms&team=facility_management";
-    var callback = function (error, response, body) {
-        var classrooms = JSON.parse(body);
-        res.render('classrooms', {"classrooms": classrooms});
-    };
-    request(url, callback);
-});
-
 // Add a new reservation
 router.route('/:id/newReservation')
     .get(function (req, res, next) {
         res.render('newReservation');
-    }).post(function (req, res, next) {
+    })
+    .post(function (req, res, next) {
 
     var url = api + "team=facility_management&function=reserveClassroom";
     var data = "id=" + req.params.id + "&day=" + req.body.section + "&section=" + req.body.section + "&timeslot=" + req.body.timeslot + "&length=" + req.body.length;
